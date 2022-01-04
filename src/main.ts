28275b9f42b52b81c1e8bc36c8ef8d2a2cca57ee
Pavel Kastornyy <pavelkastornyy@gmail.com>
2022-01-04 15:25:13 +0200

    Updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,20 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import installI18n from './lang/index'
+import { createI18n } from 'vue-i18n'
 
 loadFonts()
 
 const app = createApp(App)
 installI18n(app)
 
+const i18n = createI18n({
+  locale: 'en',
+  messages: {
+  }
+})
+
 app
-//  .use(i18n)
+  .use(i18n)
   .use(vuetify)
   .mount('#app')
